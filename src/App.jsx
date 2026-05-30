b360e5404d6f911d654491f061c53fe280cfa1c1
tavleen2008 <vinnymeaw@gmail.com>
2026-05-30 13:38:32 +0530

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-import './App.css';
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(true);
 
   return (
-    <div className="app-container">
+    <div className="flex h-screen bg-dark-bg text-white">
       <Sidebar isOpen={sidebarOpen} />
-      <div className="main-wrapper">
+      <div className="flex flex-col flex-1 overflow-hidden">
         <Header onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
         <MainContent />
       </div>
